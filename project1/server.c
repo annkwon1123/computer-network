89e0633fe36ed9a08cb67e2b72da366b54a3ddeb
Soyeon <annkwon1123@gmail.com>
2023-04-21 02:57:35 +0900

    modify#1

--- project1/server.c
+++ project1/server.c
@@ -6,20 +6,23 @@
 #include <netinet/in.h> /* has the sockaddr_in structure */
 #include <sys/socket.h> /* holds address familt and socket functions */ 
 #include <sys/wait.h> 
+#include <sys/stat.h>
 #include <arpa/inet.h>
+#include <unistd.h>
+#include <fcntl.h>
 
 #define PORT 6560 /* port through which connection is to be madea */
 #define BACKLOG 10 /* how many pendinf connections queue will hold */
 #define MSG_SIZE 1024
 
 int main() {
-	int sockfd, new_fd; /* listen on soch_fd, new connection on new_fd */
+	int sock_fd, new_fd; /* listen on sock_fd, new connection on new_fd */
 	struct sockaddr_in my_addr; /* my address */
 	struct sockaddr_in their_addr; /* connector address */
 	int sin_size;
 
-	if ((sockfd = socket(PF_INET, SOCK_STREAM, 0)) == -1) {
-		perror("socket");
+	if ((sock_fd = socket(PF_INET, SOCK_STREAM, 0)) == -1) {
+		perror("[ERR] socket");
 		exit(1);
 	}
 
@@ -27,30 +30,40 @@ int main() {
 	my_addr.sin_port = htons(PORT); /* short, network byte order */
 	my_addr.sin_addr.s_addr = htonl(INADDR_ANY); /* INADDR_ANY allowws clients to connect to any one of the host's IP address */
 
-	if (bind(sockfd, (struct sockaddr *)&my_addr, sizeof(struct sockaddr)) == -1) {
-		perror("bind");
+	if (bind(sock_fd, (struct sockaddr *)&my_addr, sizeof(struct sockaddr)) == -1) {
+		perror("[ERR] bind");
 		exit(1);
 	}
 
-	if (listen(sockfd, BACKLOG) == -1) {
-		perror("listen");
+	if (listen(sock_fd, BACKLOG) == -1) {
+		perror("[ERR] listen");
 		exit(1);
 	}
 
 	while(1) { /* main accept() loop */
 		sin_size = sizeof(struct sockaddr_in);
-		if ((new_fd = accept(sockfd, (struct sockaddr *) &their_addr, &sin_size)) == -1) {
-			perror("accept");
+		if ((new_fd = accept(sock_fd, (struct sockaddr *) &their_addr, &sin_size)) == -1) {
+			perror("[ERR] accept");
 			continue;
 		}
 
-		printf("server: got connection from %s\n", inet_ntoa(their_addr.sin_addr));
+		//printf("server: got connection from %s\n", inet_ntoa(their_addr.sin_addr));
 		
 		char request[MSG_SIZE];
+		if(read(new_fd, request, MSG_SIZE) == -1) {
+			perror("[ERR] read");
+			exit(1);
+		}
+
+		int fd = 0;
+		if(fd = open("/html.html", O_RDONLY)  == -1) {
+        	perror("[ERR] open");
+        	exit(1);
+    	}
+
 		int num_bytes = 0;
-		while((num_bytes = recv(new_fd, request, MSG_SIZE-1, 0)) > 0) {
-			request[num_bytes] = '\0';
-			printf("%s", request);
+		while((num_bytes = read(fd, request, MSG_SIZE)) > 0) {
+			write(fd, request, num_bytes);
 		}
 	}
 }
