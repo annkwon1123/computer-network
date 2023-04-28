a20dd6e2c352265a67ddae195b38c9c80feba7b9
Soyeon <annkwon1123@gmail.com>
2023-04-29 03:32:47 +0900

    handle_stat

--- submit/server.c
+++ submit/server.c
@@ -6,22 +6,27 @@
 #include <netinet/in.h> /* has the sockaddr_in structure */
 #include <sys/socket.h> /* holds address familt and socket functions */ 
 #include <sys/wait.h> 
-#include <sys/stat.h>
-//#include <arpa/inet.h> /* inet_ntoa() functions */
-#include <unistd.h>
-#include <fcntl.h>
+#include <sys/stat.h> /* stat() function */
+//#include <arpa/inet.h> /* inet_ntoa() function: get the ip address of this machine */
+#include <unistd.h> /* fork() function: make child process */
+#include <fcntl.h> /* open() function: control the file */
 
 #define BACKLOG 10 /* how many pendinf connections queue will hold */
 #define BUF_SIZE 1024
+#define CONTENT_LEN 36L
+#define CONTENT_TYPE "text/html"
+
 #define HEADER_FMT "HTTP/1.1 %d %s\nContent-Length: %ld\nContent-Type: %s\n\n"
+#define NOT_FOUND_CONTENT       "<h1>404 Not Found</h1>\n"
+#define SERVER_ERROR_CONTENT    "<h1>500 Internal Server Error</h1>\n"
 
 void http_handler(int new_fd);
-void fill_header(char *header, int status, long len, char *type);
+void handle_stat(int new_fd, char *header, int status, long len, char *type);
 void find_mime(char *ct_type, char *uri);
 
 int main(int argc, char **argv) {
 	if (argc < 2) exit(0);
-    int port = atoi(argv[1]);
+    int port = atoi(argv[1]); /* get the port number */
 	
 	int sock_fd, new_fd; /* listen on sock_fd, new connection on new_fd */
 	struct sockaddr_in my_addr; /* my address */
@@ -56,7 +61,6 @@ int main(int argc, char **argv) {
 			perror("[ERR] accept\n");
 			continue;
 		}
-	
 		//printf("[INFO] server: got connection from %s\n", inet_ntoa(their_addr.sin_addr));
 
 		int pid = fork();
@@ -79,6 +83,7 @@ void http_handler(int new_fd) {
 
 	if(read(new_fd, buf, BUF_SIZE) == -1) {
 		perror("[ERR] read request\n");
+		handle_stat(new_fd, header, 500, CONTENT_LEN, CONTENT_TYPE);
 		return;
 	}
 	printf("%s", buf);
@@ -87,11 +92,11 @@ void http_handler(int new_fd) {
     char *uri = strtok(NULL, " ");
     if (method == NULL || uri == NULL) {
         perror("[ERR] URI\n");
+		handle_stat(new_fd, header, 500, CONTENT_LEN, CONTENT_TYPE);
         return;
     }
 
-	//printf("[INFO] Handling Request: method=%s, URI=%s\n", method, uri);
-    char safe_uri[BUF_SIZE];
+	char safe_uri[BUF_SIZE];
     char *local_uri;
     struct stat st;
 
@@ -99,37 +104,46 @@ void http_handler(int new_fd) {
     if (!strcmp(safe_uri, "/")) strcpy(safe_uri, "/html.html");
     
     local_uri = safe_uri + 1;
-	stat(local_uri, &st);
+	if (stat(local_uri, &st) < 0) {
+        perror("[WARN] No file found matching URI.\n");
+        handle_stat(new_fd, header, 404, CONTENT_LEN, CONTENT_TYPE); 
+		return;
+    }
 
     int fd = open(local_uri, O_RDONLY);
     if (fd < 0) {
         perror("[ERR] open file\n");
+		handle_stat(new_fd, header, 500, CONTENT_LEN, CONTENT_TYPE);
         return;
     }
-
+	
 	int ct_len = st.st_size;
     char ct_type[40];
     find_mime(ct_type, local_uri);
-	fill_header(header, 200, ct_len, ct_type);
-    write(new_fd, header, strlen(header));
+	handle_stat(new_fd, header, 200, ct_len, ct_type);
 
     int cnt;
     while ((cnt = read(fd, buf, BUF_SIZE)) > 0)
         write(new_fd, buf, cnt);
 }
 
-void fill_header(char *header, int status, long len, char *type) {
+void handle_stat(int new_fd, char *header, int status, long len, char *type) {
     char status_text[40];
     switch (status) {
-        case 200:
+        case 200: 
             strcpy(status_text, "OK"); break;
         case 404:
-            strcpy(status_text, "Not Found"); break;
+            strcpy(status_text, "Not Found"); 
+			write(new_fd, NOT_FOUND_CONTENT, sizeof(NOT_FOUND_CONTENT));
+			break;
         case 500:
         default:
-            strcpy(status_text, "Internal Server Error"); break;
+            strcpy(status_text, "Internal Server Error"); 
+			write(new_fd, SERVER_ERROR_CONTENT, sizeof(SERVER_ERROR_CONTENT));
+			break;
     }
     sprintf(header, HEADER_FMT, status, status_text, len, type);
+	write(new_fd, header, strlen(header));
 }
 
 void find_mime(char *ct_type, char *uri) {
